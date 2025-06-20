8710090ce5ee84bd1bbe14f75a87eb416c33b3b7
Sammontalbano22 <120127489+Sammontalbano22@users.noreply.github.com>
2025-06-20 10:43:29 -0600

    Add https for security

--- StickyNoteProject/vite.config.js
+++ StickyNoteProject/vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: {
+      key: fs.readFileSync('./key.pem'),
+      cert: fs.readFileSync('./cert.pem'),
+    },
     proxy: {
-      '/api': 'http://localhost:3001',
+      '/api': 'https://localhost:3001',
     },
   },
 })
